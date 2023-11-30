0b2d8b568b09722e19642e2bdb83db25540dd979
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-11-30 15:48:16 +0100

    root branch create

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+// require ("mongoose")
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schma({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+},{ timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
