9a94673200faf762da85e6e56ce8bf362fdeee14
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-12-09 21:25:47 +0100

    connect to insomia

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ mongoose
     console.log(err);
   });
 
-let app = express();
+const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
