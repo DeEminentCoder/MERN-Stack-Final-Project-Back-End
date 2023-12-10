77f865f874dcfeb839395f5920a1c2854e5abf5c
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-12-10 23:06:30 +0100

    install bcryptjs

--- models/user.model.js
+++ models/user.model.js
@@ -1,23 +1,26 @@
 // require ("mongoose")
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schma({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  }
-},{ timestamps: true});
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
