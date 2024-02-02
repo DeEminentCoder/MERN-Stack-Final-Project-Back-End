4d36526037c775eae19d649ad9ba3ceecd35f3e7
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2024-02-02 16:24:32 +0100

    start on listing

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connected to Mongodb");
+    console.log('Connected to MongoDB!');
   })
   .catch((err) => {
     console.log(err);
   });
+
+  const __dirname = path.resolve();
 
 const app = express();
 
@@ -22,11 +26,19 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000!');
 });
 
-app.use('/api/user', userRouter)
-app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -35,5 +47,5 @@ app.use((err, req, res, next) => {
     success: false,
     statusCode,
     message,
-  })
-})
+  });
+});
